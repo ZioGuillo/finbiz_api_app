a2746d7a6d005d9ad0a4f3ea64bf5456f85f1e01
Pablo <pcisnerp@gmail.com>
2024-02-14 12:45:07 -0800

    Update CORS configuration in app.py

--- app/app.py
+++ app/app.py
@@ -23,7 +23,7 @@ app.config['WTF_CSRF_ENABLED'] = True # Sensitive
 app.config['SECRET_KEY'] = base64.b64decode(os.getenv('SECRET_KEY'))
 app.config['SESSION_TYPE'] = os.getenv('SESSION_TYPE')
 
-CORS(app)  # Enable CORS on the app
+CORS(app, resources={r"/*": {"origins": "*", "send_wildcard": "True"}}) # Sensitive
 Session(app)
 
 metrics = PrometheusMetrics(app)
