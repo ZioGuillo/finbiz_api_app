69bdbb525ca6ce93607236731e6aad6a834f868e
Pablo <pcisnerp@gmail.com>
2024-02-14 17:41:52 -0800

    Update port number in app.run()

--- app/app.py
+++ app/app.py
@@ -100,4 +100,4 @@ def request_wants_json():
 app.register_blueprint(stocks_app, url_prefix='/stocks')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
